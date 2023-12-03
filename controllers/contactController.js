2938090d2deca40eb264940fb8024ac0c354904d
Rashi Gupta <rashiguptaim@gmail.com>
2023-12-03 16:06:57 +0530

    logged in user can fetch contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,9 @@
 const asyncHandler=require("express-async-handler");
 const Contact=require("../models/contactsModel");
 const getAllContact=asyncHandler(async(req,res)=>{
-    const contacts=await Contact.find();
+    const contacts=await Contact.find({
+        user_id:req.user.id,
+    });
     res.status(200).json({
         contacts,
     })
@@ -15,7 +17,8 @@ const createContact=asyncHandler(async(req,res)=>{
     const contact=await Contact.create({
         name,
         email,
-        phone
+        phone,
+        user_id:req.user.id
     });
     res.status(201).json({
       contact
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,7 +1,8 @@
 const express=require("express");
-const {getAllContact,createContact,getContact,updateContact,deleteContact} =require("../controllers/contactController")
+const {getAllContact,createContact,getContact,updateContact,deleteContact} =require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenHandler");
 const router=express.Router();
-
+router.use(validateToken);
 router.route("/").get(getAllContact).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
